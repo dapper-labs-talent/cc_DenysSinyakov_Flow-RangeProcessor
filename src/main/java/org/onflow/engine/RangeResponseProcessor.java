a50731b2c2d093af415a53821acf7df4c21162fd
denyssinyakov <hello@wealthx.ai>
2022-07-16 12:14:26 -0700

    processRange method added;

--- src/main/java/org/onflow/engine/RangeResponseProcessor.java
+++ src/main/java/org/onflow/engine/RangeResponseProcessor.java
@@ -1,4 +1,19 @@
 package org.onflow.engine;
 
+import java.util.Map;
+
 public class RangeResponseProcessor {
+  private final Map<Long, Integer> heightToProcessedBlocks;
+
+  public RangeResponseProcessor(Map<Long, Integer> heightToProcessedBlocks) {
+    this.heightToProcessedBlocks = heightToProcessedBlocks;
+  }
+
+  public void processRange(long startHeight, Block[] blocks) {
+    long i = startHeight;
+    for (Block block : blocks) {
+      heightToProcessedBlocks.merge(i, 1, Integer::sum);
+      i++;
+    }
+  }
 }
--- src/test/java/org/onflow/engine/RangeResponseProcessorTest.java
+++ src/test/java/org/onflow/engine/RangeResponseProcessorTest.java
@@ -2,6 +2,10 @@ package org.onflow.engine;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
@@ -9,8 +13,27 @@ class RangeResponseProcessorTest {
 
   @Test
   void rangeProcessorCanBeCreated() {
-    RangeResponseProcessor r = new RangeResponseProcessor();
+    RangeResponseProcessor r = new RangeResponseProcessor(new HashMap<>());
     assertNotNull(r);
+  }
+
+  @Test
+  void processRangeCanProcessConsecutiveRangeOfBlocks() {
+    Block[] blocks = new Block[]{
+        new Block("first"),
+        new Block("second"),
+        new Block("third")
+    };
+    long startHeight = 0;
+    Map<Long, Integer> heightToProcessedBlocks = new HashMap<>();
+    RangeResponseProcessor processor = new RangeResponseProcessor(heightToProcessedBlocks);
+    processor.processRange(startHeight, blocks);
+    Map<Long, Integer> expectedResult = new HashMap<>(){{
+      put(0L, 1);
+      put(1L, 1);
+      put(2L, 1);
+    }};
+    assertEquals(expectedResult, heightToProcessedBlocks);
   }
 
 }
