be8095c7237a874c0158208b77f46a41dba717fc
denyssinyakov <hello@wealthx.ai>
2022-07-16 12:18:03 -0700

    test for several Consecutive blocks

--- src/test/java/org/onflow/engine/RangeResponseProcessorTest.java
+++ src/test/java/org/onflow/engine/RangeResponseProcessorTest.java
@@ -18,7 +18,7 @@ class RangeResponseProcessorTest {
   }
 
   @Test
-  void processRangeCanProcessConsecutiveRangeOfBlocks() {
+  void processRangeCanProcessSingleBlock() {
     Block[] blocks = new Block[]{
         new Block("first"),
         new Block("second"),
@@ -32,6 +32,27 @@ class RangeResponseProcessorTest {
       put(0L, 1);
       put(1L, 1);
       put(2L, 1);
+    }};
+    assertEquals(expectedResult, heightToProcessedBlocks);
+  }
+
+  @Test
+  void processRangeCanProcessConsecutiveRangeOfBlocks() {
+    Block[] blocks = new Block[]{
+        new Block("first"),
+        new Block("second"),
+        new Block("third")
+    };
+    Map<Long, Integer> heightToProcessedBlocks = new HashMap<>();
+    RangeResponseProcessor processor = new RangeResponseProcessor(heightToProcessedBlocks);
+    processor.processRange(0, blocks);
+    processor.processRange(1, blocks);
+    processor.processRange(1, blocks);
+    Map<Long, Integer> expectedResult = new HashMap<>(){{
+      put(0L, 1);
+      put(1L, 3);
+      put(2L, 3);
+      put(3L, 2);
     }};
     assertEquals(expectedResult, heightToProcessedBlocks);
   }
